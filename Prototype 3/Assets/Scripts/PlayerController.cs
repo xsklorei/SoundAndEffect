201309678b2b0533a4a9f9ae089af89a9287bc40
xsklorei <jcvxka@gmail.com>
2023-03-23 11:39:57 -0700

    added script
    player controller to step 4 end

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 100, ForceMode.Impulse);
+        }
+    }
+}
