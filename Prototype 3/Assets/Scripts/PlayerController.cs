208030ed9108392aa74740b3163d3f3b72ed6cfb
Jcvxka <jcvxka@gmail.com>
2023-04-19 21:48:45 -0700

    added script

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,11 @@ public class PlayerController : MonoBehaviour
     public ParticleSystem explosionParticle;
     public AudioClip crashSound;
     public AudioClip jumpSound;
+    public bool doubleJumpUsed = false;
+    public float doubleJumpForce;
+    public bool doubleSpeed = false;
     private AudioSource playerAudio;
+
 
 
     // Start is called before the first frame update
@@ -37,7 +41,28 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+            doubleJumpUsed = false;
         }
+        else if(Input.GetKeyDown(KeyCode.Space) && !isOnground && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }    
+
     }
 
     public bool gameOver;
--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(20, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    private int randomObstacle;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,9 @@ public class SpawnManager : MonoBehaviour
     {
         if(playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
+
         }
         
     }
